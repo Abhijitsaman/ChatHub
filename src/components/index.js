9e3743caff6a0b53b50e7a41f04b8fdd7bb09e43
Abhijit Samanta <samantaabhijit354@gmail.com>
2026-08-23 20:57:46 +0530

    Export components from index.js

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Avatar } from './Avatar';
+export { default as LoadingScreen } from './LoadingScreen';
+export { default as MessageActions } from './MessageActions';
+export { default as MessageBubble } from './MessageBubble';
+export { default as MessageInfo } from './MessageInfo';
+export { default as ProtectedRoute } from './ProtectedRoute';
