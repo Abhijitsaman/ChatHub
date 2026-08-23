c87836d4ddc751b6cc7cbfac55bf2514dca44cd4
Abhijit Samanta <samantaabhijit354@gmail.com>
2026-08-23 20:58:15 +0530

    Add index.js to export multiple services

---
+++ src/services/index.js
@@ -0,0 +1,7 @@
+export { userService } from './userService';
+export { conversationService } from './conversationService';
+export { messageService } from './messageService';
+export { callService } from './callService';
+export { blockService } from './blockService';
+export { FirebaseService } from './firebaseService';
+export { WebRTCService } from './webrtcService';
